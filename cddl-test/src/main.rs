785eeba5a641059bfd2030576ba2a2f8eb06b698
Matt Schwennesen <mjschwenne@gmail.com>
2025-08-14 10:38:42 -0500

    more cddl testing, begin more in-depth cbor primer

--- cddl-test/src/main.rs
+++ cddl-test/src/main.rs
@@ -1,42 +1,15 @@
-use std::fs::File;
-
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize, Debug)]
-struct PersonalData {
-    first_name: String,
-    family_name: String,
-    display_name: Option<String>,
-    age: Option<u64>,
-    // test: String,
-    shoesize: u64,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct MinimalPersonalData {
-    first_name: String,
-    family_name: String,
-}
+use cddl::{self, validate_cbor_from_slice};
 
 fn main() {
-    let person = PersonalData {
-        first_name: "Matthew".to_string(),
-        family_name: "Schwennesen".to_string(),
-        display_name: Some("Matt".to_string()),
-        age: Some(24),
-        // test: "test".to_string(),
-        shoesize: 11,
-    };
-    let min_person = MinimalPersonalData {
-        first_name: "Matthew".to_string(),
-        family_name: "Schwennesen".to_string(),
-    };
+    let test_f64: f64 = 32.0;
 
-    let output_file = File::create("person.cbor").unwrap();
-    let _ = ciborium::into_writer(&person, output_file);
-    let serialized = serde_json::to_string(&person).unwrap();
-    println!("JSON = {}", serialized);
+    let mut encoded: Vec<u8> = Vec::new();
+    let _ = ciborium::into_writer(&test_f64, &mut encoded);
 
-    let m_output_file = File::create("mperson.cbor").unwrap();
-    let _ = ciborium::into_writer(&min_person, m_output_file);
+    let cddl_f16 = r#"testrule = float16"#;
+    let cddl_f32 = r#"testrule = float32"#;
+    let cddl_f64 = r#"testrule = float64"#;
+    assert!(validate_cbor_from_slice(cddl_f16, &encoded, Some(&["cbor"])).is_ok());
+    assert!(validate_cbor_from_slice(cddl_f32, &encoded, Some(&["cbor"])).is_ok());
+    assert!(validate_cbor_from_slice(cddl_f64, &encoded, Some(&["cbor"])).is_ok());
 }
