cb11f0dc21c4083590c9da210f88b49c826cf3f3
Matt Schwennesen <mjschwenne@gmail.com>
2026-01-12 15:22:20 -0600

    clean up report repo, split into subfiles

--- flake.nix
+++ flake.nix
@@ -8,21 +8,25 @@
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = {
-    nixpkgs,
-    flake-utils,
-    cddl,
-    rust-overlay,
-    ...
-  }:
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      cddl,
+      rust-overlay,
+      ...
+    }:
     flake-utils.lib.eachDefaultSystem (
-      system: let
-        overlays = [(import rust-overlay)];
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
           inherit system overlays;
         };
-      in {
-        devShells.default = with pkgs;
+      in
+      {
+        devShells.default =
+          with pkgs;
           mkShell {
             buildInputs = [
               # For protobuf experiments
@@ -36,34 +40,10 @@
               rust-bin.stable.latest.default
 
               # LaTeX
-              (texlive.combine {
-                inherit
-                  (texlive)
-                  scheme-medium
-                  biblatex
-                  biber
-                  geometry
-                  wrapfig
-                  multirow
-                  booktabs
-                  capt-of
-                  titlesec
-                  hyperref
-                  amsmath
-                  mathtools
-                  enumitem
-                  mathpartir
-                  xcolor
-                  tcolorbox
-                  bbold
-                  minted
-                  upquote
-                  ;
-              })
+              texlive.combined.scheme-full
             ];
 
-            shellHook = ''
-            '';
+            shellHook = '''';
           };
       }
     );
